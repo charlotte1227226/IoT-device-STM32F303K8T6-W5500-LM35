8503a4e117ba63e2337818f08e5474a7b1383f7d
charlotte1227226 <andy20011019@gmail.com>
2025-05-11 15:41:00 +0800

    version 1

---
+++ Core/Inc/w5500_spi.h
@@ -0,0 +1,18 @@
+/**
+******************************************************************************
+* @file    w5500.h
+* @author  NTUT Chung Po An
+* @brief   Header file of W5500 spi driver  
+******************************************************************************
+*/
+#ifndef __W5500_SPI_H
+#define __W5500_SPI_H
+#include <stdio.h>
+#include "main.h"
+#include "usart.h"
+#include "spi.h"
+#include "gpio.h"
+
+
+
+#endif
