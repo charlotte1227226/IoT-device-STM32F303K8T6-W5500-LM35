bbdd2d46e85aba4455a93b3c56ea2700e41f5980
charlotte1227226 <andy20011019@gmail.com>
2025-05-11 16:43:57 +0800

    version 1

--- Core/Inc/w5500_spi.h
+++ Core/Inc/w5500_spi.h
@@ -7,12 +7,27 @@
 */
 #ifndef __W5500_SPI_H
 #define __W5500_SPI_H
-#include <stdio.h>
-#include "main.h"
-#include "usart.h"
+#include "stm32f3xx_hal.h"
 #include "spi.h"
 #include "gpio.h"
 
+typedef enum
+{
+    SPI_OK = 0,
+    SPI_ERROR = 1,
+    SPI_TIMEOUT = 2,
+    SPI_BUSY = 3,
+} SPI_StatusTypeDef;
 
+/*Definition of spi id*/
+#define SPI1_ID 1
+#define SPI2_ID 2
+#define SPI3_ID 3
+/*end of definition*/
 
+#define SPI_TIMEOUT_VALUE 1000 //ms
+
+SPI_StatusTypeDef SPI_Init(SPI_HandleTypeDef *hspi);
+SPI_StatusTypeDef SPI_Write(SPI_HandleTypeDef *hspi, uint8_t *reg_addr, uint8_t *pData, uint16_t Size, uint32_t addr_size);
+SPI_StatusTypeDef SPI_Read(SPI_HandleTypeDef *hspi, uint8_t *reg_addr, uint8_t *pData, uint16_t Size, uint32_t addr_size);
 #endif
